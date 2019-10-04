e94d13330874aadf08d1d047d07500431390cd77
abuisson <a.buisson@acropole-expert.com>
2019-10-04 15:48:23 +0200

    adding pages and background color

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,36 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Home.scss'
 
 class Home extends React.PureComponent {
   render() {
     return (
-      <div>
+      <div className="Home">
         <div className="overlay" id="particles"></div>
-        <div className="home container">
+        <div className="home-container">
           <div className="content">
             <div className="title">
               <h1>Hello !</h1>
             </div>
-            <ul>
-              <li>
-                <Link className='js-open-about' href="#">
-                  <i className='pe-7s-user'></i>
-                  <span>About Me</span>
-                </Link>
-              </li>
-              <li>
-                <Link className='js-open-works' href="#">
-                  <i className='pe-7s-albums'></i>
-                  <span>My Works</span>
-                </Link>
-              </li>
-              <li>
-                <Link className='js-open-contact' href="#">
-                  <i className='pe-7s-chat'></i>
-                  <span>Contact Me</span>
-                </Link>
-              </li>
-            </ul>
+              <Link className='js-open-about' href="#">
+                <i className='pe-7s-user'></i>
+                <span>About Me</span>
+              </Link>
+
+              <Link className='js-open-works' href="#">
+                <i className='pe-7s-albums'></i>
+                <span>My Works</span>
+              </Link>
+              <Link className='js-open-contact' href="#">
+                <i className='pe-7s-chat'></i>
+                <span>Contact Me</span>
+              </Link>
           </div>
           <footer>
             <p> Alexandre Buisson &copy; 2019</p>
